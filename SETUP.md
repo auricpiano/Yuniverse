e6a4771e9153de537ea64e08d0f5c37c76ecfb34
auricpiano <52860490+auricpiano@users.noreply.github.com>
2023-05-21 00:19:39 +0900

    [YUN-2] Add design library (#2)
    * Clear
    
    * Init
    
    * Add dependencies for tunnel
    
    * Add react-native-paper
    
    * Add babel plugin for native-paper
    
    * Fix version for compatibility with expo
    
    * try using rnp

---
+++ SETUP.md
@@ -0,0 +1,6 @@
+nvm install 18.16.0
+nvm use 18
+npm install -g yarn
+
+yarn create expo-app Yuniverse
+yarn expo start --tunnel
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
+    env: {
+      production: {
+        // To get smaller bundle size by excluding modules you don't use, you can use our optional babel plugin.
+        // The plugin automatically rewrites the import statements so that only the modules you use are imported instead of the whole library.
+        plugins: ['react-native-paper/babel'],
+      },
+    },
   };
 };
