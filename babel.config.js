9917294af9dd2a8b80fb8aa2d142752e5fb6f434
auricpiano <auricpiano@gmail.com>
2023-05-20 20:41:16 +0900

    Add babel plugin for native-paper

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,12 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    env: {
+      production: {
+        // To get smaller bundle size by excluding modules you don't use, you can use our optional babel plugin.
+        // The plugin automatically rewrites the import statements so that only the modules you use are imported instead of the whole library.
+        plugins: ['react-native-paper/babel'],
+      },
+    },
   };
 };
